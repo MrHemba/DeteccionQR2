b71ed44f93266453171d8a332c2035a93e3e893a
Mr_HeMbA <119627674+MrHemba@users.noreply.github.com>
2024-08-22 11:18:06 -0500

    Actualizacion

--- app/src/main/java/com/example/deteccionqr/MainActivity.java
+++ app/src/main/java/com/example/deteccionqr/MainActivity.java
@@ -1,15 +1,36 @@
 package com.example.deteccionqr;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.mlkit.vision.barcode.BarcodeScanner;
+import com.google.mlkit.vision.barcode.BarcodeScanning;
+import com.google.mlkit.vision.barcode.common.Barcode;
+import com.google.mlkit.vision.common.InputImage;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private ImageView imageView;
+    private TextView textViewResult;
+    private BarcodeScanner barcodeScanner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +41,50 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        imageView = findViewById(R.id.imageView);
+        textViewResult = findViewById(R.id.textView_result);
+        Button buttonSelectImage = findViewById(R.id.button_select_image);
+
+        barcodeScanner = BarcodeScanning.getClient();
+
+        buttonSelectImage.setOnClickListener(v -> {
+            Intent intent = new Intent(Intent.ACTION_PICK);
+            intent.setType("image/*");
+            startActivityForResult(intent, PICK_IMAGE_REQUEST);
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            if (data != null && data.getData() != null) {
+                Uri imageUri = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    imageView.setImageBitmap(bitmap);
+                    scanBarcode(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+    private void scanBarcode(Bitmap bitmap) {
+        InputImage image = InputImage.fromBitmap(bitmap, 0);
+
+        barcodeScanner.process(image)
+                .addOnSuccessListener(barcodes -> {
+                    for (Barcode barcode : barcodes) {
+                        String rawValue = barcode.getRawValue();
+                        textViewResult.setText(rawValue);
+                    }
+                })
+                .addOnFailureListener(e -> textViewResult.setText("No se pudo escanear el código."));
     }
 }
